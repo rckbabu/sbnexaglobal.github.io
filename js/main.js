d0dcbed67f3e459820ac4fee29aedc5fb6476024
rckbabu <rckbabu@gmail.com>
2026-07-07 09:54:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-document.addEventListener('DOMContentLoaded',()=>console.log('SB Nexa Global'));
+window.addEventListener("scroll",function(){
+
+const navbar=document.querySelector(".navbar");
+
+if(window.scrollY>50){
+
+navbar.style.background="#081f3f";
+
+navbar.style.boxShadow="0 4px 10px rgba(0,0,0,.2)";
+
+}
+
+else{
+
+navbar.style.background="#0d2b52";
+
+navbar.style.boxShadow="none";
+
+}
+
+});
